9e9f7e33c49c295a1c1719303d50909b4704efe5
Collin Bardini <cbardini@osisoft.com>
2021-11-15 11:16:59 -0800

    updates (#7)

---
+++ appsettings.py
@@ -0,0 +1,43 @@
+from ocs_sample_library_preview import OCSClient
+import json
+
+# Settings
+stream_query = '"SLTCSensorUnit1"'
+asset_query = '"SLTC Sensor1"'
+data_view_id = 'SLTC Sensor Unit'
+prefix = ''  # prefix for streams, assets, etc.
+test_prefix = 'SAMPLE_TEST:'  # prefix for testing
+max_stream_count = 150  # The maximum number of streams to copy
+max_asset_count = 150  # The maximum number of assets to copy
+start_time = '2021-05-01'  # Time window of values to transfer
+end_time = '2021-05-02'
+request_timeout = 30  # The request timeout limit
+
+# Read configuration
+try:
+    with open(
+        'appsettings.json',
+        'r',
+    ) as f:
+        appsettings = json.load(f)
+except Exception as error:
+    print(f'Error: {str(error)}')
+    print(f'Could not open/read appsettings.json')
+    exit()
+
+source_sds_source = OCSClient(appsettings.get('SourceConfiguration').get('ApiVersion'),
+                              appsettings.get('SourceConfiguration').get('TenantId'),
+                              appsettings.get('SourceConfiguration').get('Resource'),
+                              appsettings.get('SourceConfiguration').get('ClientId'),
+                              appsettings.get('SourceConfiguration').get('ClientSecret'))
+source_sds_source.request_timeout = request_timeout
+
+destination_sds_source = OCSClient(appsettings.get('DestinationConfiguration').get('ApiVersion'),
+                                   appsettings.get('DestinationConfiguration').get('TenantId'),
+                                   appsettings.get('DestinationConfiguration').get('Resource'),
+                                   appsettings.get('DestinationConfiguration').get('ClientId'),
+                                   appsettings.get('DestinationConfiguration').get('ClientSecret'))
+destination_sds_source.request_timeout = request_timeout
+
+source_namespace_id = appsettings.get('SourceConfiguration').get('NamespaceId')
+destination_namespace_id = appsettings.get('DestinationConfiguration').get('NamespaceId')
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import unittest
-import configparser
-import config
+import appsettings
 
 from ocs_sample_library_preview import DataItemResourceType, SdsResultPage, OCSClient
 from concurrent.futures import ThreadPoolExecutor
@@ -20,60 +19,60 @@ class SDSPythonSampleTests(unittest.TestCase):
 def cleanup():
 
     # Settings
-    stream_query = f'"{config.test_prefix}SLTCSensorUnit1"'
-    asset_query = f'Name:"SLTC Sensor1" AND AssetTypeId:"{config.test_prefix}"*'
-    data_view_id = f'{config.test_prefix}SLTC Sensor Unit'
+    stream_query = f'"{appsettings.test_prefix}SLTCSensorUnit1"'
+    asset_query = f'Name:"SLTC Sensor1" AND AssetTypeId:"{appsettings.test_prefix}"*'
+    data_view_id = f'{appsettings.test_prefix}SLTC Sensor Unit'
 
     # Find the data view with the specified id and delete it
     try:
-        config.destination_sds_source.DataViews.deleteDataView(
-            namespace_id=config.destination_namespace_id, data_view_id=data_view_id)
+        appsettings.destination_sds_source.DataViews.deleteDataView(
+            namespace_id=appsettings.destination_namespace_id, data_view_id=data_view_id)
     except Exception as ex:
         print((f"Encountered Error: {ex}"))
         print
 
     # Find all assets that have the test prefix and delete them
-    assets = config.destination_sds_source.Assets.getAssets(
-        namespace_id=config.destination_namespace_id, query=asset_query, skip=0, count=config.max_asset_count)
+    assets = appsettings.destination_sds_source.Assets.getAssets(
+        namespace_id=appsettings.destination_namespace_id, query=asset_query, skip=0, count=appsettings.max_asset_count)
     asset_type_ids = set()
     for asset in assets:
         asset_type_ids.add(asset.AssetTypeId)
-        config.destination_sds_source.Assets.deleteAsset(
-            namespace_id=config.destination_namespace_id, asset_id=asset.Id)
+        appsettings.destination_sds_source.Assets.deleteAsset(
+            namespace_id=appsettings.destination_namespace_id, asset_id=asset.Id)
 
     # Delete all asset types used by created assets
     for asset_type_id in asset_type_ids:
         # Check that the type id is not still in use
-        assets = config.destination_sds_source.Assets.getAssets(
-            namespace_id=config.destination_namespace_id, query=f'AssetTypeId:"{asset_type_id}"')
+        assets = appsettings.destination_sds_source.Assets.getAssets(
+            namespace_id=appsettings.destination_namespace_id, query=f'AssetTypeId:"{asset_type_id}"')
 
         if len(assets) > 0:
             print(
                 f'Asset type use {asset_type_id} still in use! It will be skipped.')
         else:
             # Delete type
-            config.destination_sds_source.Assets.deleteAssetType(
-                namespace_id=config.destination_namespace_id, asset_type_id=asset_type_id)
+            appsettings.destination_sds_source.Assets.deleteAssetType(
+                namespace_id=appsettings.destination_namespace_id, asset_type_id=asset_type_id)
 
     # Find all streams that have the test prefix and delete them
-    streams = config.destination_sds_source.Streams.getStreams(
-        namespace_id=config.destination_namespace_id, query=stream_query, skip=0, count=config.max_stream_count)
+    streams = appsettings.destination_sds_source.Streams.getStreams(
+        namespace_id=appsettings.destination_namespace_id, query=stream_query, skip=0, count=appsettings.max_stream_count)
     type_ids = set()
     for stream in streams:
         type_ids.add(stream.TypeId)
-        config.destination_sds_source.Streams.deleteStream(
-            namespace_id=config.destination_namespace_id, stream_id=stream.Id)
+        appsettings.destination_sds_source.Streams.deleteStream(
+            namespace_id=appsettings.destination_namespace_id, stream_id=stream.Id)
 
     # Find all types associated with the created streams and delete them
     for type_id in type_ids:
         try:
-            config.destination_sds_source.Types.deleteType(
-                namespace_id=config.destination_namespace_id, type_id=type_id)
+            appsettings.destination_sds_source.Types.deleteType(
+                namespace_id=appsettings.destination_namespace_id, type_id=type_id)
         except Exception as ex:
             print((f"Encountered Error: {ex}"))
             print
 
 
 if __name__ == '__main__':
-    
+
     unittest.main()
